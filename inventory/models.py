51c99ad1430c37b8243380b66e59f6108063481d
ershum <97314127+OutTray@users.noreply.github.com>
2022-01-14 18:30:21 -0500

    2022/01/14

--- inventory/models.py
+++ inventory/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Inventory_db(models.Model):
-    item_id = models.CharField(max_length = 255, blank = True, unique = True)
     item_name = models.CharField(max_length = 255)
     item_quantity = models.IntegerField(default = 0)
 
